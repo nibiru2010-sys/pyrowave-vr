7d5e6f9520c3cf16202702efde63f38d6138c04b
Hans-Kristian Arntzen <post@themaister.net>
2026-09-02 14:59:28 +0200

    Add packet loss simulation to the viewer.

--- viewer.cpp
+++ viewer.cpp
@@ -14,6 +14,7 @@
 #include "ui_manager.hpp"
 #include <string.h>
 #include <stdexcept>
+#include <random>
 
 using namespace Granite;
 using namespace Vulkan;
@@ -91,6 +92,7 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 
 	bool is_mouse_active = false;
 	bool paused = false;
+	bool first_decode = true;
 
 	enum class Mode
 	{
@@ -99,6 +101,17 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 		Delta
 	};
 	Mode mode = Mode::Slide;
+	bool lossy_simulation = false;
+	int lossy_rate = 1;
+	struct
+	{
+		uint64_t decode_success_with_mask;
+		uint64_t decode_success_no_mask;
+		uint64_t decode_success_perfect;
+		uint64_t decode_failure;
+	} simulated = {};
+	std::mt19937 rnd;
+	std::uniform_int_distribution<int> dist{0, 1000};
 
 	bool on_mouse(const MouseMoveEvent &e)
 	{
@@ -127,6 +140,18 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 				mode = Mode::Delta;
 			else if (e.get_key() == Key::S)
 				mode = Mode::Slide;
+			else if (e.get_key() == Key::L)
+				lossy_simulation = !lossy_simulation;
+			else if (e.get_key() == Key::Right)
+			{
+				lossy_rate = std::min<int>(lossy_rate + 1, 100);
+				simulated = {};
+			}
+			else if (e.get_key() == Key::Left)
+			{
+				lossy_rate = std::max<int>(lossy_rate - 1, 1);
+				simulated = {};
+			}
 			else if (e.get_key() == Key::P)
 			{
 				get_wsi().set_backbuffer_format(
@@ -165,6 +190,8 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 			enc.init(&e.get_device(), file.get_width(), file.get_height(), chroma);
 			dec.init(&e.get_device(), file.get_width(), file.get_height(), chroma, device_should_use_fragment_path(e.get_device()));
 		}
+
+		first_decode = true;
 	}
 
 	void on_device_destroyed(const DeviceCreatedEvent &)
@@ -270,6 +297,10 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 
 		unsigned bitstream_size = bit_rate_mbit * 1000000ull / (60 * 8);
 
+		std::vector<uint32_t> active_mask_buffer;
+		constexpr int PristineBands = 3;
+		constexpr float MinimumPacketRatio = 0.9f;
+
 		if (!rawfile)
 		{
 			bitstream_size &= ~3u;
@@ -332,11 +363,63 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 				return;
 			}
 
+			for (int bands = 1; bands < 5; bands++)
+			{
+				size_t num_active_blocks = enc.get_num_active_blocks(bands);
+				active_mask_buffer.resize((num_active_blocks + 31) / 32);
+				enc.compute_block_active_words(bands, active_mask_buffer.data(), active_mask_buffer.size(),
+				                               mapped_meta);
+
+				uint32_t active_blocks = 0;
+				for (auto &word : active_mask_buffer)
+					active_blocks += Util::popcount32(word);
+
+				LOGI("Bands %u -> residency ratio (%u / %zu) %.3f %%, critical packets %zu\n", bands, active_blocks,
+				     num_active_blocks, 100.0f * float(active_blocks) / float(num_active_blocks),
+				     enc.compute_num_critical_packets(bands, mapped_meta, 8 * 1024));
+			}
+
 			assert(out_packets == num_packets);
 
+			std::vector<bool> lossy_successes(packets.size());
+
 			for (auto &p: packets)
+			{
+				if (lossy_simulation)
+					if (lossy_rate > dist(rnd))
+						continue;
+
+				lossy_successes[&p - packets.data()] = true;
+
 				if (!dec.push_packet(reordered_packet_buffer.data() + p.offset, p.size))
 					return;
+			}
+
+			size_t num_active_blocks = enc.get_num_active_blocks(PristineBands);
+			active_mask_buffer.resize((num_active_blocks + 31) / 32);
+			enc.compute_block_active_words(PristineBands, active_mask_buffer.data(), active_mask_buffer.size(),
+										   mapped_meta);
+
+			if (lossy_simulation)
+			{
+				size_t num_critical_packets = enc.compute_num_critical_packets(PristineBands, mapped_meta, 8 * 1024);
+
+				// Simulate a simple XOR FEC model. If all but one packet is received, we can recover.
+				size_t lossy_critical_successes = 0;
+				for (size_t i = 0; i < num_critical_packets; i++)
+					if (lossy_successes[i])
+						lossy_critical_successes++;
+
+				// If we're missing one critical packet, and we successfully got the XOR packet, we can reconstruct
+				// the missing packet.
+				if (lossy_critical_successes == num_critical_packets - 1 && lossy_rate <= dist(rnd))
+				{
+					LOGI("Simulating FEC success.\n");
+					// Push all critical packets again.
+					for (size_t i = 0; i < num_critical_packets; i++)
+						dec.push_packet(reordered_packet_buffer.data() + packets[i].offset, packets[i].size);
+				}
+			}
 
 			cmd = device.request_command_buffer();
 		}
@@ -346,20 +429,37 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 			if (device_should_use_fragment_path(device))
 			{
 				cmd->image_barrier(*out_images.images[i],
-				                   VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+				                   first_decode ? VK_IMAGE_LAYOUT_UNDEFINED : VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL,
+				                   VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
 				                   VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0,
 				                   VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT);
 			}
 			else
 			{
 				cmd->image_barrier(*out_images.images[i],
-				                   VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL,
+				                   first_decode ? VK_IMAGE_LAYOUT_UNDEFINED : VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL, VK_IMAGE_LAYOUT_GENERAL,
 				                   VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0,
 				                   VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT, VK_ACCESS_2_SHADER_STORAGE_WRITE_BIT);
 			}
 		}
 
-		dec.decode(*cmd, out_images.views);
+		first_decode = false;
+
+		if (dec.decode_is_ready(true, PristineBands, MinimumPacketRatio,
+		                        active_mask_buffer.data(), active_mask_buffer.size()))
+		{
+			if (dec.decode_is_ready(true, PristineBands, MinimumPacketRatio, nullptr, 0))
+				simulated.decode_success_no_mask++;
+			if (dec.decode_is_ready(false))
+				simulated.decode_success_perfect++;
+			dec.decode(*cmd, out_images.views);
+			simulated.decode_success_with_mask++;
+		}
+		else
+		{
+			LOGI("Dropped frame due to simulated packet loss.\n");
+			simulated.decode_failure++;
+		}
 
 		for (int i = 0; i < 3; i++)
 		{
@@ -394,6 +494,18 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 		x_slide = clamp(x_slide, 50, int(cmd->get_viewport().width) - 50);
 
 		const float full_color = get_wsi().get_backbuffer_format() == BackbufferFormat::HDR10 ? 0.75f : 1.0f;
+
+		double simulated_success_rate =
+			simulated.decode_failure ?
+			double(simulated.decode_success_with_mask) / double(simulated.decode_success_with_mask + simulated.decode_failure) : 1.0;
+
+		double simulated_success_rate_no_mask =
+			simulated.decode_failure ?
+			double(simulated.decode_success_no_mask) / double(simulated.decode_success_with_mask + simulated.decode_failure) : 1.0;
+
+		double simulated_success_rate_perfect =
+			simulated.decode_failure ?
+			double(simulated.decode_success_perfect) / double(simulated.decode_success_with_mask + simulated.decode_failure) : 1.0;
 
 		if (mode == Mode::Flicker && !rawfile)
 		{
@@ -450,10 +562,14 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 			if (!rawfile)
 			{
 				flat_renderer.begin();
-				char text[64];
-				snprintf(text, sizeof(text), "%u mbits | %.3f bpp @ 60 fps%s", bit_rate_mbit,
+				char text[256];
+				snprintf(text, sizeof(text), "%u mbits | %.3f bpp @ 60 fps%s | Loss rate %.1f %% | Success rate (with mask = %.3f %%, no mask = %.3f %%, perfect = %.3f %%)",
+				         bit_rate_mbit,
 				         double(bitstream_size * 8) / double(file.get_width() * file.get_height()),
-				         paused ? " (paused)" : "");
+				         paused ? " (paused)" : "", lossy_simulation ? lossy_rate * 0.1 : 0,
+				         simulated_success_rate * 100.0, simulated_success_rate_no_mask * 100.0,
+				         simulated_success_rate_perfect * 100.0);
+
 				flat_renderer.render_text(GRANITE_UI_MANAGER()->get_font(UI::FontSize::Large),
 				                          text, vec3(20, 20, 0), vec2(400, 200),
 				                          vec4(full_color, full_color, 0.0f, 1.0f),
@@ -474,11 +590,16 @@ struct ViewerApplication : Granite::Application, Granite::EventHandler
 			cmd->draw(3);
 
 			flat_renderer.begin();
-			char text[64];
-			snprintf(text, sizeof(text), "DELTA %u mbits | %.3f bpp @ 60 fps%s",
-					 bit_rate_mbit,
-					 double(bitstream_size * 8) / double(file.get_width() * file.get_height()),
-					 paused ? " (paused)" : "");
+			char text[256];
+
+			snprintf(text, sizeof(text),
+			         "DELTA %u mbits | %.3f bpp @ 60 fps%s | Loss rate %.1f %% | Success rate (with mask = %.3f %%, no mask = %.3f %%, perfect = %.3f %%)",
+			         bit_rate_mbit,
+			         double(bitstream_size * 8) / double(file.get_width() * file.get_height()),
+			         paused ? " (paused)" : "", lossy_simulation ? lossy_rate * 0.1 : 0.0,
+			         simulated_success_rate * 100.0, simulated_success_rate_no_mask * 100.0,
+			         simulated_success_rate_perfect * 100.0);
+
 			flat_renderer.render_text(GRANITE_UI_MANAGER()->get_font(UI::FontSize::Large),
 									  text, vec3(20, 20, 0), vec2(400, 200),
 									  vec4(full_color, full_color, 0.0f, 1.0f),
